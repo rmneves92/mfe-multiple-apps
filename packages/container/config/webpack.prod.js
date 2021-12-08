e25b6b32a2498c76de0d1b74553ce48bbc1b8984
Rafael Neves <rafa.mneves@hotmail.com>
2021-12-07 22:43:34 -0300

    fix container deploy script

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -4,7 +4,6 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const domain = process.env.PRODUCTION_DOMAIN;
-//
 const prodConfig = {
   mode: 'production',
   output: {
