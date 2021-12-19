d861f2d8542b9a15becff5d21bfd2b6a99732945
Rafael Neves <rafa.mneves@hotmail.com>
2021-12-19 18:03:53 -0300

    add publicPath

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-import Signin from 'components/Signin';
-import Signup from 'components/Signup';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'au',
@@ -15,8 +15,8 @@ export default ({ history }) => {
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
           <Switch>
-            <Route path="/auth/signin" component="Signin" />
-            <Route path="/auth/signup" component="Signup" />
+            <Route path="/auth/signin" component={Signin} />
+            <Route path="/auth/signup" component={Signup} />
           </Switch>
         </Router>
       </StylesProvider>
